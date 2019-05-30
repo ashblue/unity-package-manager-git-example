aa22cabd18f486498a637b29e14df34ec812de3a
Ash Blue <ash@blueashes.com>
2019-05-29 22:12:26 -0600

    Restructured project to conforms to Unity's recommended structure standards
    https://docs.unity3d.com/Manual//cus-layout.html

--- ExampleProject/Assets/Scripts/CalculatorWrapper.cs
+++ ExampleProject/Assets/Scripts/CalculatorWrapper.cs
@@ -1,11 +1,4 @@
-﻿using UnityEngine;
+﻿using CleverCrow.UnityPackageGitExample;
 
-public class CalculatorWrapper : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start () {
-    }
-
-    // Update is called once per frame
-    void Update () {
-    }
+public class CalculatorWrapper : Calculator {
 }
